1929e3fded08bcfb3852fb040503592c2391e41f
txt1994 <linguangyuan88@gmail.com>
2023-07-11 18:26:27 +0800

    提交插件

---
+++ File.h
@@ -0,0 +1,23 @@
+/**
+  ************************ Copyright *************** 
+  *           (C) Copyright {Year},txt1994,China, GCU.
+  *                  All Rights Reserved
+  *
+  *				https://github.com/txt1994
+  *				email:linguangyuan88@gmail.com
+  *				QQ	 :2643989637
+  *      
+  * FileName     : {FileName}   
+  * Version      : v1.0		
+  * Author       : txt1994			
+  * Date         : {Date}         
+  * Description  :    
+  * Function List:  
+  * Toolchain : RealView Development Suite
+  *             RealView Microcontroller Development Kit (MDK)
+  *             ARM Developer Suite (ADS)
+  *             Keil uVision
+  ******************************************************
+**/
+
+{ifndef}
---
+++ function.c
@@ -0,0 +1,9 @@
+/**
+  * @Name    {FunctionName}
+  * @brief  
+  * @param   {Parameters}
+  * @retval
+  * @author  txt1994 
+  * @Data    {Date}
+  * <description> :
+ **/
